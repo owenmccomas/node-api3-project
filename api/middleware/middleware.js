9e344f930918297ddf50acf8d0b577311a072199
Owen McComas <owen@mccomas.me>
2023-02-22 16:07:23 -0700

    wip

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,30 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString()
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`[${timestamp}] ${method} ${url}`)
+  next()
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUserId')
+  next()
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser')
+  next()
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validatePost')
+  next()
 }
 
 // do not forget to expose these functions to other modules
+
+module.exports = { logger, validateUserId, validatePost, validateUser }
